11ebdb01b6a273a1fb5341e6f3766915e2db3101
Jason Reeves <jhr@unitedentropy.com>
2018-01-10 16:57:36 +0000

    changed directory structure, added raipc script

--- scripts/init_dj_project.sh
+++ scripts/init_dj_project.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 name=$1
-docker-compose run web django-admin.py startproject $name .
-sudo chown -R $USER:$USER $name manage.py
-cat stubs/local_settings.py >> $name/settings.py
+docker-compose run web django-admin.py startproject $name 
+sudo chown -R $USER:$USER $name 
+cp stubs/requirements.txt $name
+cat stubs/local_settings.py >> $name/$name/settings.py
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+app=$1
+echo "Starting app: [$app]"
+cd $app
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
