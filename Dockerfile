a502c2a4826a62e9cd0aef11784f5d25a85b3696
Jason Reeves <jhr@unitedentropy.com>
2018-01-11 20:27:31 +0000

    increased python version, excluded Python from requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.4
+FROM python:3.5.1
 
 ENV PYTHONUNBUFFERED 1
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,7 @@
 app=$1
 echo "Starting app: [$app]"
 cd $app
+cat requirements.txt | grep -v "Python==" > /tmp/requirements.txt 
+pip install -r /tmp/requirements.txt
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
